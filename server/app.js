efd2d511eb0d5202cc6f1e9411bcbd6f9805e6e0
mebra005 <mebra005@fiu.edu>
2019-04-25 08:23:26 -0700

    Replace var with let

--- server/app.js
+++ server/app.js
@@ -10,20 +10,20 @@ const baseURL = "https://badapi.iqvia.io/api/v1/Tweets";
 const startDate = "2016-01-01T00:00:00.001Z";
 const endDate = "2017-12-31T23:59:59.999Z";
 
-var allTweets = [];
-var uniqueTweets = new Set([]);
-var tweetCounts = 0;
-var uniqueTweetCount = 0;
-var duplicateTweets = 0;
+let allTweets = [];
+let uniqueTweets = new Set([]);
+let tweetCounts = 0;
+let uniqueTweetCount = 0;
+let duplicateTweets = 0;
 
-var URL = `${baseURL}?startDate=${startDate}&endDate=${endDate}`;
+let URL = `${baseURL}?startDate=${startDate}&endDate=${endDate}`;
 
 //Sends request to receive tweets between two dates
 async function getTweets() {
   try {
 
     //Sending request to API
-    var response = await axios.get(URL);
+    let response = await axios.get(URL);
 
     //Adding the first 100 tweets to array in first response
     allTweets = allTweets.concat(response.data);
@@ -38,11 +38,11 @@ async function getTweets() {
         //new start date is equal to the last tweet's date 
         //on the previuse response and replacing the current miliseconds to 9999
         //to avoid duplicate 
-        var nextStartDate = response.data[response.data.length - 1].stamp;
+        let nextStartDate = response.data[response.data.length - 1].stamp;
         nextStartDate = nextStartDate.substring(0, nextStartDate.indexOf('.')) + ".9999";
 
         //modifying the url with the newStartDate
-        var nextURL = `${baseURL}?startDate=${nextStartDate}&endDate=${endDate}`;
+        let nextURL = `${baseURL}?startDate=${nextStartDate}&endDate=${endDate}`;
         
         //Sending the modified request to API to get the next portion of tweets 
         response = await axios.get(nextURL);
@@ -72,7 +72,7 @@ async function getTweets() {
 
 
     //Save the results back to file
-    var file = fs.createWriteStream('tweets.txt');
+    let file = fs.createWriteStream('tweets.txt');
     file.on('error', function (err) { console.log(err) });
     file.write(`Unique Tweets: ${tweetCounts}\r\nDuplicate tweets: ${duplicateTweets}` + '\r\n\r\n')
     allTweets.forEach(function (t) { file.write(`id: ${t.id}\r\ntext: ${t.text}\r\nstamp: ${t.stamp}\r\n` + '\r\n'); });
